bc913a19f8c9f6a4ca0357494610a653c81e82c5
Sonder82 <Himrs82@gmail.com>
2021-07-10 13:25:30 +0500

    3.3. Разбор первой программы. [#159033]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Aleksey Novoselov");
+        System.out.println("28.02.1982");
+    }
+}
+
