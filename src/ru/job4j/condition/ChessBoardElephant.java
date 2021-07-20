50180ae9eb594e414a19943bd44c513a5d4539f6
Sonder82 <Himrs82@gmail.com>
2021-07-20 18:10:54 +0500

    4.3.6. Math.abs [#373616]

---
+++ src/ru/job4j/condition/ChessBoardElephant.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class ChessBoardElephant {
+    public static int way(int x1, int y1, int x2, int y2) {
+        int rsl = 0;
+        if (Math.abs(x1 - x2) == Math.abs(y1 - y2) && x1 >= 0 && x1  < 8 && y1 >= 0 && y1 < 8 && x2 >= 0 && x2  < 8 && y2 >= 0 && y2 < 8) {
+            rsl = Math.abs(x1 - x2);
+        }
+        return rsl;
+
+    }
+}
