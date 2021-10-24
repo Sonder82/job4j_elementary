64f97f7ffb4f9b41f6539ea786f03e9634b85802
Sonder82 <Himrs82@gmail.com>
2021-10-24 16:47:43 +0500

    0.2. Кидаем исключение - throw new RuntimeException [#219365]

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -11,7 +11,7 @@ public class Fact {
             throw new IllegalArgumentException("n should be more than zero.");
         }
         int rsl = 1;
-        for (int index = 1; index <= n; index++) {
+        for (int index = 1; index < n; index++) {
             rsl *= index;
         }
         return rsl;
