8fbfbcaea6058be42c26f36bf2074d6d88a0f363
Sonder82 <Himrs82@gmail.com>
2021-10-26 14:36:43 +0500

    0.4. Пользовательские исключения. [#219367 #220664]

--- src/main/java/ru/job4j/ex/ElementNotFoundException.java
+++ src/main/java/ru/job4j/ex/ElementNotFoundException.java
@@ -4,29 +4,4 @@ public class ElementNotFoundException extends Exception {
     public ElementNotFoundException(String message) {
         super(message);
     }
-
-    public static class FindEl {
-        public static int indexOf(String[] value, String key) throws ElementNotFoundException {
-            int rsl = -1;
-            for (int index = 0; index < value.length; index++) {
-                if (value[index].equals(key)) {
-                    rsl = index;
-                }
-            }
-            if (rsl == -1) {
-                throw new ElementNotFoundException("Element not found");
-            }
-            return rsl;
-        }
-    }
-
-    public static void main(String[] args) {
-        try {
-            String[] color = new String[]{"black", "white", "red"};
-            System.out.println(FindEl.indexOf(color, "white"));
-        } catch (ElementNotFoundException e) {
-            e.printStackTrace();
-        }
-
-    }
 }
